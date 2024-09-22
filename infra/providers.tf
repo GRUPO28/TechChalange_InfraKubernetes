68c32328025f33000ea68179e04451d95d4420fa
victorg-duarte <victorgduarte00@gmail.com>
2024-09-22 15:55:22 -0300

    add bucket s3 k8s terraform.tfstate

--- infra/providers.tf
+++ infra/providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "github-pipe"
+    key    = "k8s/terraform.tfstate"
+    region = "sa-east-1"
+  }
 }
 
 provider "aws" {
