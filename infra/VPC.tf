d07964b9411cb30ff1ca4fbb26352a7c130d5511
victorg-duarte <victorgduarte00@gmail.com>
2024-09-22 15:40:53 -0300

    add connection db vpc

--- infra/VPC.tf
+++ infra/VPC.tf
@@ -1,3 +1,18 @@
+
+data "aws_vpc" "default-vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["default-us-east-1"]
+  }
+}
+
+resource "aws_vpc_peering_connection" "peer_vpc_default_connection" {
+  vpc_id        = module.vpc.vpc_id # source
+  peer_vpc_id   = data.aws_vpc.default-vpc.id # destination
+
+  depends_on = [module.vpc]
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.13.0"
